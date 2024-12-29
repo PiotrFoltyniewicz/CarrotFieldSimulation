ec281524d04f16260463b1113c3212eb227c290d
KacperKuznik <kuznikkacper@gmail.com>
2024-12-29 01:59:26 +0100

    getters and setters

--- src/general/GameManager.java
+++ src/general/GameManager.java
@@ -1,5 +1,23 @@
 package general;
+import general.SimulationGUI;
+import simulation.environment.Field;
+import simulation.entities.Entity;
+import java.util.*;
 
 public class GameManager {
+    private Field field;
+    private SimulationGUI renderer;
+    private List<Entity> entities;
 
+    public void startSimulation(){
+		System.out.println("starting");
+	}
+
+    public void manageTurn() {
+        
+    }
+    
+    public Field getField() {
+        return field;
+    }
 }
