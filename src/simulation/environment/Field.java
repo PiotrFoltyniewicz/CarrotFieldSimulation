9793d0151f4e2c342825ba785bb745db66b0c7c5
Piotr Foltyniewicz <piotr.foltyniewicz@gmail.com>
2024-12-29 22:53:06 +0100

    added Field constructor, but Tile is still empty

--- src/simulation/environment/Field.java
+++ src/simulation/environment/Field.java
@@ -5,6 +5,17 @@ public class Field {
     private Tile[][] tiles;
     private int fieldSize;
 
+    public Field(int fieldSize) {
+        this.fieldSize = fieldSize;
+        tiles = new Tile[fieldSize][fieldSize];
+
+        for (int i = 0; i < fieldSize; i++) {
+            for (int j = 0; j < fieldSize; j++) {
+                tiles[i][j] = new Tile();
+            }
+        }
+    }
+
     public int getFieldSize() {
         return fieldSize;
     }
